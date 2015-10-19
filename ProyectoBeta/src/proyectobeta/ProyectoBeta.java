c430c703be408c5460db6d36fec8b91f09609264
jbarrosgarcia <jbarrosgarcia@atanasoff07.danielcastelao.org>
2015-10-19 09:06:47 +0200

    elimine comentarios en proyecto beta

---
+++ ProyectoBeta/src/proyectobeta/ProyectoBeta.java
@@ -0,0 +1,15 @@
+
+
+package proyectobeta;
+
+
+public class ProyectoBeta {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Estamos probando cosas");
+        
+        // TODO code application logic here
+    }
+    
+}
