0aef811956efb675229c380c93f913a3928b49e4
jbarrosgarcia <jbarrosgarcia@atanasoff07.danielcastelao.org>
2015-10-29 09:38:35 +0100

    Cambie el funcionamiento del programa

--- ProyectoBeta/src/proyectobeta/ProyectoBeta.java
+++ ProyectoBeta/src/proyectobeta/ProyectoBeta.java
@@ -1,13 +1,15 @@
 
-
 package proyectobeta;
-
+import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class ProyectoBeta {
 
     
     public static void main(String[] args) {
-        System.out.println("Estamos probando cosas");
+        String prueba="Estamos probando cosas";
+        JOptionPane.showMessageDialog(null, prueba);
+        String ventana=JOptionPane.showInputDialog("Probando cosas random");
         
        
     }
